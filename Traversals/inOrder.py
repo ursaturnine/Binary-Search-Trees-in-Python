9e9642684a2022fe68e271863cdccc996406ba36
Tyrah <tyrah96@gmail.com>
2023-02-10 15:12:30 -0800

    in order traversal

---
+++ Traversals/inOrder.py
@@ -0,0 +1,34 @@
+"""DFS: In order Traversal of Binary Tree.
+
+1. Traverse the left subtree, i.e, call Inorder(left -> right)
+2. Visit the root
+3. Traverse the right subtree, i.e, call Inorder(right -> left)
+"""
+
+
+class Node:
+    def __init__(self, key):
+        self.data = key
+        self.left = None
+        self.right = None
+    
+def inOrderTraversal(root):
+
+    if root:
+
+        inOrderTraversal(root.left)
+
+        print(root.data)
+
+        inOrderTraversal(root.right)
+
+if __name__== '__main__':
+
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+
+    print('Inorder traversal of a binary tree is')
+    inOrderTraversal(root)
